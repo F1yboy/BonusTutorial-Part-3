5df12d5946a68b8f1a2c09dcafa98ee40b0a9a5d
F1yboy <42953928+F1yboy@users.noreply.github.com>
2018-12-09 11:58:17 -0500

    Add files via upload

---
+++ Script/NPCMove.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NPCMove : MonoBehaviour {
+
+    [SerializeField]
+    Transform _destination;
+
+    NavMeshAgent _navMeshAgent;
+
+	void Start () 
+    {
+        _navMeshAgent = this.GetComponent<NavMeshAgent>();
+
+        if(_navMeshAgent == null)
+        {
+            Debug.LogError("The nav mesh agent component is not attatched to " + gameObject.name);
+        }
+        else
+        {
+            SetDestination();
+        }
+	}
+
+    private void Update()
+    {
+        if (Input.GetKey("escape"))
+            Application.Quit();
+    }
+
+    private void SetDestination()
+    {
+        if(_destination != null)
+        {
+            Vector3 targetVector = _destination.transform.position;
+            _navMeshAgent.SetDestination(targetVector);
+        }
+    }
+}
